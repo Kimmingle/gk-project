9550898c51dec842bad95b4dc1b20cb4149853dc
김민아 <110553201+Kimmingle@users.noreply.github.com>
2025-12-02 21:21:18 +0900

    test2

--- gk_bk/src/main/java/org/gk/website/user/controller/UserController.java
+++ gk_bk/src/main/java/org/gk/website/user/controller/UserController.java
@@ -19,6 +19,6 @@ public class UserController {
     @PostMapping
     public ResponseEntity<?> login(@RequestBody UserLoginRequest request) { //클라이언트가 보낸 json을 읽기 위해 userLoginRequest 객체로 매핑.
         //UserLoginResponse result = userService.login(request);
-        return ResponseEntity.ok("result");
+        return ResponseEntity.ok("result~");
     }
 }
